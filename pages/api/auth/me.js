edfe8dc4d84c717a43ca2a51c97b8487a30d2a1d
zulfaeuis1326 <zulfaeuis1326@gmail.com>
2026-08-12 15:23:36 +0800

    Update me.js

--- pages/api/auth/me.js
+++ pages/api/auth/me.js
@@ -3,6 +3,7 @@ const { getUserFromReq } = require("../../../lib/auth");
 
 export default async function handler(req, res) {
   try {
+    res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
     await ensureSchema();
     const user = await getUserFromReq(req);
     return res.status(200).json({ user });
